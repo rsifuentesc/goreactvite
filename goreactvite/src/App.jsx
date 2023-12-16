25012be57308daf154f8231187ebfd042a5dbd13
Ricardo Sifuentes Carranza <rsifuentesc@gmail.com>
2023-12-16 01:05:40 -0500

    first commit

---
+++ goreactvite/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function App() {
+  return (
+<div>
+    <h1>Hello world</h1>
+    <button onClick={ async ()=>{
+        const response = await fetch('http://localhost:3000/users')
+        const data = await response.json()
+        console.log(data)
+    }}>Obterner datos</button>
+</div>
+  )
+}
+
+export default App
