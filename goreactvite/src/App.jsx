633e8a54de058cb20afad32a87fba1b2cc1e2037
rsifuentesc <53318804+rsifuentesc@users.noreply.github.com>
2023-12-20 08:13:14 -0500

    Update App.jsx

--- goreactvite/src/App.jsx
+++ goreactvite/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 <div>
     <h1>Hello world</h1>
     <button onClick={ async ()=>{
-        const response = await fetch('http://localhost:3000/users')
+        const response = await fetch('http://localhost:8080/users')
         const data = await response.json()
         console.log(data)
     }}>Obterner datos</button>
